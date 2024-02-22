3db59983e9c58fb44af0608244559472a37fc8c8
Peter Onsomu <pkonsomu2020@gmail.com>
2024-02-22 17:05:11 +0300

    Final Push

---
+++ user_input.py
@@ -0,0 +1,13 @@
+# WEEKLY CODE CHALLENGE
+
+# Asks the user for their name and stores the varibale in name
+name = input("What is your name?")
+
+# Asks the user for their age and stores it in the variable
+age = input("How old are you?")
+
+# Asks the user for their location and stores it in the variable
+location = input("Where do you live?")
+
+# Prints out a personalized message using the user's name, age, and location
+print("Hello {}, you are {} years old and live in {}." .format(name, age, location))
